191d50437e57e1e5a562631b215ed9849464d138
Sylvain <sc96@hotmail.fr>
2025-12-05 15:21:39 +0100

    Mise a jour background

--- src/components/Header/layout.tsx
+++ src/components/Header/layout.tsx
@@ -6,7 +6,8 @@ type LayoutProps = {
 }
 
 const Layout = ({ children }: LayoutProps) => (
-	<div className="bg-base-200 min-h-screen">
+	// Todo create a gradient for a bg-base-200 to a darker version 
+	<div className="bg-base-100 min-h-screen">
 		<Header />
 		<main className="flex justify-center relative z-10">
 			{children}
